dd9a44b1ead8b32665e82cb079209f9bc27386fa
Somesh Tiwari <someshtiwari.in@gmail.com>
2026-09-03 00:49:33 +0530

    Add solution for Climbing Stairs

---
+++ problems/climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        // here we make the platform for dp array 
+        vector<int>dp(n+1,-1);
+        // initialed with -1
+        if(n==1){
+            return 1;
+
+        }
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+        
+    }
+};
