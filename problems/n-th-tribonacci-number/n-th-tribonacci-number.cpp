b1fe00bbdba0002c2a0371d7ef29e47b1de049c3
Somesh Tiwari <someshtiwari.in@gmail.com>
2026-09-13 02:36:53 +0530

    Add solution for N-th Tribonacci Number

--- problems/n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ problems/n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -6,17 +6,14 @@ public:
         arr[0]=0;
         arr[1]=1;
         arr[2]=1;
-        if(n<=2){
-            return 2;
-        }
-        else{
+       
             // means greater than 2 
-            for(int i=3;i<n-1;i++){
+            for(int i=3;i<n;i++){
                 arr[i]=arr[i-3]+arr[i-2]+arr[i-1];
             }
 
-            return arr[n-1];
-        }
+            return arr[n];
+        
         
     }
 };
