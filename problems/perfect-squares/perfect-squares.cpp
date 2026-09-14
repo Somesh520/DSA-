bc4ef9288cc38775c9546229d4a053f927e4933d
Somesh Tiwari <someshtiwari.in@gmail.com>
2026-09-14 10:53:01 +0530

    Add solution for Perfect Squares

--- problems/perfect-squares/perfect-squares.cpp
+++ problems/perfect-squares/perfect-squares.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
+    
 
-    int solve(int n){
+    int solve(int n,vector<int>&arr){
         // here we write the frist conditon 
         if(n==0){
             return 0;
         }
-        int mincount=INT_MIN;
-        for(int i=0;i*i<=n;i++){
+        if(arr[n]!=-1){
+            return arr[n];
+        }
+        int mincount=INT_MAX;
+        for(int i=1;i*i<=n;i++){
             // now here 
-            int result=1+solve(n-1);
+            int result=1+solve(n-i*i,arr);
             mincount=min(mincount,result);
         }
-        return mincount;
+        return arr[n]=mincount;
     }
     int numSquares(int n) {
         // let solve this problem 
         //first we build the story 
         // make helper function for this 
-        int helper=solve(n);
+        vector<int>arr(n+1,-1);
+        int helper=solve(n,arr);
         return helper;
         
     }
